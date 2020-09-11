7a5c08442aef190c324db8dd8e09922054488569
j3ffh95 <j3ffh95@gmail.com>
2020-09-10 20:02:48 -0400

    Added updateVisitCOunt funtion to make a call to the api

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const countElement = document.querySelector("#count");
+
+function updateVisitCount() {
+  fetch("https://api.countapi.xyz/update/john-doe/youtube/?amount=1")
+    .then((res) => res.json())
+    .then((res) => {
+      countElement.innerHTML = res.value;
+    });
+}
